c7510e6d2a2b7eb330c5a97d412a856395b27441
BuildTools <unconfigured@null.spigotmc.org>
2024-01-29 16:47:19 -0500

    Initial Creation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://support.bloxlane.devs-stuff.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/support/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
