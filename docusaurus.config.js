2611fb3b0aae5491c76571e63757e3a9058b6aab
BuildTools <unconfigured@null.spigotmc.org>
2024-01-29 16:56:06 -0500

    Initial Creation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: "bloxlane", // Usually your GitHub org/user name.
   projectName: "support", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
